acac7259db73b234b76847b61e5c3ee54a4d60c2
Mai Bảo Long <66893806+zitsieunhan1512@users.noreply.github.com>
2023-02-23 02:54:34 +0700

    fix bug: fix multi dot and multi operator!

--- app/src/main/java/com/mblhcmute/calculatorproapp/MainActivity.java
+++ app/src/main/java/com/mblhcmute/calculatorproapp/MainActivity.java
@@ -14,6 +14,7 @@ public class MainActivity extends AppCompatActivity {
     private TextView tvInput;
     private boolean lastNumeric = false;
     private boolean lastDot = false;
+    private boolean singledot = false;
     private ActivityMainBinding binding;
 
     @Override
@@ -23,6 +24,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(binding.getRoot());
 
         tvInput = binding.tvInput;
+        tvInput.setText("");
     }
 
     public void onDigit(View view) {
@@ -33,26 +35,91 @@ public class MainActivity extends AppCompatActivity {
 
     public void onClear(View view) {
         tvInput.setText("");
+        lastNumeric = false;
+        lastDot = false;
+        singledot = false;
     }
+
     public void onRemove(View view) {
-        if (tvInput.getText().length() > 0) {
-            tvInput.setText(tvInput.getText().subSequence(0, tvInput.getText().length() - 1));
-        }
-    }
-    public void onDecimalPoint(View view) {
-        if (lastNumeric && !lastDot) {
-            tvInput.append(".");
-            lastNumeric = false;
-            lastDot = true;
+        String text = tvInput.getText().toString();
+        if (text.length() > 1) {
+            tvInput.setText(text.subSequence(0, text.length() - 1));
+            String tmp = String.valueOf(text.charAt(text.length() - 1));
+            text = tvInput.getText().toString();
+            String lastChar = String.valueOf(text.charAt(text.length() - 1));
+            if (lastChar.equals(".")) {
+                lastNumeric = false;
+                lastDot = true;
+                singledot = true;
+            } else if (lastChar.equals("+") || lastChar.equals("-")
+                    || lastChar.equals("*") || lastChar.equals("/") || lastChar.equals("%")) {
+                lastNumeric = false;
+                lastDot = false;
+                singledot = false;
+            } else if (lastChar.charAt(0) >= '0' && lastChar.charAt(0) <= '9') {
+                lastNumeric = true;
+                lastDot = false;
+                int operatorCount = countOperator(text);
+                int dotCount = countDots(text);
+                if (text.startsWith("-")) {
+                    operatorCount -= 1;
+                }
+                if (singledot && tmp.equals(".") && (operatorCount>=dotCount)) {
+                    singledot = false;
+                }
+                if (!singledot && !tmp.equals(".") && (dotCount>0) && (operatorCount ==0)) {
+                    singledot = true;
+                }
+            }
+        } else {
+            onClear(view);
         }
     }
 
+    public void onDecimalPoint(View view) {
+        if (!singledot) {
+            if (lastNumeric && !lastDot) {
+                tvInput.append(".");
+                lastNumeric = false;
+                lastDot = true;
+                singledot = true;
+            }
+        }
+    }
+
+    public int countDots(String s) {
+        int count = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '.') {
+                count++;
+            }
+        }
+        return count;
+    }
+
+
     public void onOperator(View view) {
-        if (lastNumeric && !isOperatorAdded(tvInput.getText().toString())) {
+        String text = tvInput.getText().toString();
+        if (((Button) view).getText().equals("-") && text.equals("")) {
+            tvInput.append(((Button) view).getText());
+            singledot = false;
+        }
+        if (lastNumeric && !isOperatorAdded(text) && countOperator(text) < 2) {
             tvInput.append(((Button) view).getText());
             lastNumeric = false;
             lastDot = false;
+            singledot = false;
         }
+    }
+
+    public int countOperator(String s) {
+        int count = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '+' || s.charAt(i) == '-' || s.charAt(i) == '*' || s.charAt(i) == '/' || s.charAt(i) == '%') {
+                count++;
+            }
+        }
+        return count;
     }
 
     public void onEqual(View view) {
@@ -96,7 +163,7 @@ public class MainActivity extends AppCompatActivity {
                         one = prefix + one;
                     }
                     tvInput.setText(removeZeroAfterDot(Double.toString(Double.parseDouble(one) / Double.parseDouble(two))));
-                }else if (tvValue.contains("%")) {
+                } else if (tvValue.contains("%")) {
                     String[] splitValue = tvValue.split("%");
                     String one = splitValue[0];
                     String two = splitValue[1];
